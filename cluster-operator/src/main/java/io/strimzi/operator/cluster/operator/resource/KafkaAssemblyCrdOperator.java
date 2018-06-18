a8b6a349f5391ef08c14f59a7fd98d95e9ebc44f
Tom Bentley <tbentley@redhat.com>
2018-06-18 09:48:45 +0100

    Add KafkaAssemblyCrdOperator

---
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/resource/KafkaAssemblyCrdOperator.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2018, Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.operator.cluster.operator.resource;
+
+import io.fabric8.kubernetes.api.model.apiextensions.CustomResourceDefinition;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.MixedOperation;
+import io.fabric8.kubernetes.client.dsl.Resource;
+import io.strimzi.operator.cluster.crd.DoneableKafkaAssembly;
+import io.strimzi.operator.cluster.crd.KafkaAssemblyList;
+import io.strimzi.operator.cluster.crd.model.KafkaAssembly;
+import io.vertx.core.Vertx;
+
+public class KafkaAssemblyCrdOperator extends AbstractResourceOperator<KubernetesClient, KafkaAssembly, KafkaAssemblyList, DoneableKafkaAssembly, Resource<KafkaAssembly, DoneableKafkaAssembly>> {
+    /**
+     * Constructor
+     * @param vertx The Vertx instance
+     * @param client The Kubernetes client
+     */
+    public KafkaAssemblyCrdOperator(Vertx vertx, KubernetesClient client) {
+        super(vertx, client, KafkaAssembly.RESOURCE_KIND);
+    }
+
+    @Override
+    protected MixedOperation<KafkaAssembly, KafkaAssemblyList, DoneableKafkaAssembly, Resource<KafkaAssembly, DoneableKafkaAssembly>> operation() {
+        CustomResourceDefinition crd = client.customResourceDefinitions().withName(KafkaAssembly.RESOURCE_NAME).get();
+        return client.customResources(crd, KafkaAssembly.class, KafkaAssemblyList.class, DoneableKafkaAssembly.class);
+    }
+}
